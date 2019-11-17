fe669d9ac9736cdcbd3bf3b12b7e83aa26796f4a
AFrunze <45385572+AFrunze@users.noreply.github.com>
2019-11-17 23:18:10 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,14 +1,20 @@
 --В сформированной базе данных составьте запрос, возвращающий имена эмитентов, чьи
 --облигации торгуются на МБ в режиме основных торгов, и по каждой из которых доля дней,
---когда котировка [BID или ASK выберете самостоятельно] не существовала, составляла бы не
---более 10% наблюдений за бумагой. Таблица должна содержать поля issuer, ISIN и nun_ratio
+--когда котировка [BID или ASK выберете самостоятельно] не существовала,
+-- составляла бы не более 10% наблюдений за бумагой. Таблица должна содержать поля issuer, ISIN и nun_ratio
 --(долю наблюдений без пропуска котировки). (2 балла)
 
 -- котировка ASK = 0 считаем, что она не существовала, значений NULL в ASK - нет.
 select "ISSUER","ISIN","nun_ratio"
-from ( select "ISSUER","ISIN",
-     ((select Count("ASK")* 100 from public.public.quotes_task where public.quotes_task."ASK" <> 0  Group By "ISIN")/ (select Count("ASK")* 100 from public.public.quotes_task Group By "ISIN")) as "nun_ratio" 
+from (select "ISSUER","ISIN",
+          ((select Count("ASK")* 100 
+          from public.public.quotes_task 
+          where public.quotes_task."ASK" = 0  Group By "ISIN")
+          / (select Count("ASK")* 100 
+             from public.public.quotes_task 
+             Group By "ISIN")) as "nun_ratio" 
       where "nun_ratio" < 10
      )
 where "ISIN" in ( select "ISIN" from public.listing_task
-                 where public.listing_task."Platform" like "Московская Биржа " and public.listing_task."Section" like "Основной");
+                 where public.listing_task."Platform" like "Московская Биржа " 
+                 and public.listing_task."Section" like "Основной");
