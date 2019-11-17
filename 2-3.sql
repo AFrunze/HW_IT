2b1ff62ddc934f70c309d709f2ecc524619e40bc
AFrunze <45385572+AFrunze@users.noreply.github.com>
2019-11-17 22:22:29 +0300

    Update 2-3.sql

--- 2-3.sql
+++ 2-3.sql
@@ -4,8 +4,8 @@ INSERT INTO public.listing_task ("IssuerName", "IssuerName_NRD", "IssuerOKPO")
    FROM public.bond_description_task
    WHERE public.listing_task."ISIN" = public.bond_description_task."ISINCode";
  
--- вставка данных о площадках 
-INSERT INTO public.listing_task ("IssuerName", "IssuerName_NRD", "IssuerOKPO")
-   SELECT "IssuerName","IssuerName_NRD","IssuerOKPO"
-   FROM public.bond_description_task
-   WHERE public.listing_task."ISIN" = public.bond_description_task."ISINCode";
+-- вставка данных о площадках, пусть это будет BOARDID и BOARDNAME
+INSERT INTO public.listing_task ("BOARDID", "BOARDNAME")
+   SELECT "BOARDID", "BOARDNAME"
+   FROM public.quotes_task
+   WHERE public.listing_task."ISIN" = public.bquotes_task."ISIN";
