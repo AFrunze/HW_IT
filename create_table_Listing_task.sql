9b0edcc9019e88c74cb6bc8b36d72942cc0ed8df
AFrunze <45385572+AFrunze@users.noreply.github.com>
2019-11-17 21:11:19 +0300

    Update create_table_Listing_task.sql

--- create_table_Listing_task.sql
+++ create_table_Listing_task.sql
@@ -14,7 +14,7 @@ CREATE TABLE public.listing_task
     "ID" bigint NOT NULL,
     "ISIN" text COLLATE pg_catalog."default" NOT NULL,
     "Platform" text COLLATE pg_catalog."default" NOT NULL,
-    "Section" text NOT NULL,
+    "Section" text COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT listing_task_pkey PRIMARY KEY ("ID")
 )
 
