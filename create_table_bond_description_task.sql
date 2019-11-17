6d14d5e071a3ad1c0a61d72cb3a49f3ca46b20c9
AFrunze <45385572+AFrunze@users.noreply.github.com>
2019-11-17 20:14:43 +0300

    Update create_table_bond_description_task.sql

--- create_table_bond_description_task.sql
+++ create_table_bond_description_task.sql
@@ -1,1 +1,6 @@
 
+
+
+
+-- copy public.listing_task  FROM '/Users/a1/Desktop/IT для финансистов/дз1/Data/Облигации/bond_description_task.csv' DELIMITER ';' CSV HEADER ENCODING 'WIN 1251';
+--\copy public.listing_task  FROM '/Users/a1/Desktop/IT для финансистов/дз1/Data/Облигации/bond_description_task.csv' DELIMITER ';' CSV HEADER ENCODING 'WIN 1251';
