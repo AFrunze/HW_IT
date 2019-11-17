3098d995bee76aa32f798c7036a49bc480ce4f64
AFrunze <45385572+AFrunze@users.noreply.github.com>
2019-11-17 22:11:57 +0300

    Update 2-3.sql

--- 2-3.sql
+++ 2-3.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.listing_task ("IssuerName", "IssuerName_NRD", "IssuerOKPO")
    SELECT "IssuerName","IssuerName_NRD","IssuerOKPO"
    FROM public.bond_description_task
-   WHERE public.listing_task = public.bond_description_task;
+   WHERE public.listing_task."ISIN" = public.bond_description_task."ISINCode";
