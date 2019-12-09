290f31a45970d11211e3a0501672d7c7a8083ab1
AFrunze <45385572+AFrunze@users.noreply.github.com>
2019-12-09 14:40:40 +0300

    Update create_table_bond_description_task.sql

--- create_table_bond_description_task.sql
+++ create_table_bond_description_task.sql
@@ -1,30 +1,31 @@
--- сохранить файл Excel в формате csv, все форматы дат сделать именно датами,числа числами (0 и 1 в boolean), удалить лишние (скрытые) листы из файла, отобразить все столбцы
+-- сохранить файл Excel в формате csv, все форматы дат сделать именно датами,числа числами (0 и 1 в boolean), 
+удалить лишние (скрытые) листы из файла, отобразить все столбцы
 -- импортировать с помощью функции copy данные из файла, указать откуда (FROM) копировать данные.
 -- У меня не вышло (из папки tmp тоже), поэтому с помощью  IMPORT/EXPORT: 
--- Вверху строчка EXPORT, двигаешь ползунок на IMPORT
+-- Вверху строчка EXPORT,ползунок на IMPORT
 -- Далее выбрать файл, переходя в папку, где находится документ или сразу вставить путь
 -- Он должен быть в формате csv
--- Далее сдвигаешь ползунок в HEADER 
--- После этого нужно выбирать делиметр: обычно это точка с запятой, у меня запятая
--- В строчке ENCONDING выбирать WIN1251 
+-- Далее ползунок в HEADER 
+-- После этого нужно выбирать делиметр: обычно это точка с запятой
+-- В строчке ENCONDING выбирать WIN1251 / utf8
 -- И после всех этих действий нажать ОК 
 
 CREATE TABLE public.bond_description
 (
-    "ISIN, RegCode, NRDCode" text NOT NULL,
-    "FinToolType" text,
-    "SecurityType" text,
-    "SecurityKind" text,
-    "CouponType" text,
-    "RateTypeNameRus_NRD" text,
-    "CouponTypeName_NRD" text,
-    "HaveOffer" boolean,
-    "AmortisedMty" boolean,
-    "MaturityGroup" text,
-    "IsConvertible" boolean,
-    "ISINCode" text,
-    "Status" text,
-    "HaveDefault" boolean,
+    "ISIN, RegCode, NRDCode" text COLLATE pg_catalog."default" NOT NULL,,
+    "FinToolType" text COLLATE pg_catalog."default" NOT NULL,,
+    "SecurityType" text COLLATE pg_catalog."default",
+    "SecurityKind" text COLLATE pg_catalog."default",
+    "CouponType" text COLLATE pg_catalog."default",
+    "RateTypeNameRus_NRD" text COLLATE pg_catalog."default",
+    "CouponTypeName_NRD" text COLLATE pg_catalog."default",
+    "HaveOffer" boolean NOT NULL,
+    "AmortisedMty" boolean NOT NULL,
+    "MaturityGroup" text COLLATE pg_catalog."default",
+    "IsConvertible" boolean NOT NULL,
+    "ISINCode" text COLLATE pg_catalog."default" NOT NULL,
+    "Status" text COLLATE pg_catalog."default",
+    "HaveDefault" boolean NOT NULL,
     "IsLombardCBR_NRD" boolean,
     "IsQualified_NRD" boolean,
     "ForMarketBonds_NRD" boolean,
@@ -36,8 +37,8 @@ CREATE TABLE public.bond_description
     "Coupon_Period_Base_ID" integer,
     "AccruedintCalcType" boolean,
     "IsGuaranteed" boolean,
-    "GuaranteeType" text,
-    "GuaranteeAmount" text,
+    "GuaranteeType" text COLLATE pg_catalog."default",
+    "GuaranteeAmount" text COLLATE pg_catalog."default",
     "GuarantVal" bigint,
     "Securitization" text,
     "CouponPerYear" integer, 
@@ -53,12 +54,12 @@ CREATE TABLE public.bond_description
     "FaceValue_M" integer,
     "FaceValue_NRD" real,
     "CurrentFaceValue_NRD" real,
-    "BorrowerName" text,
+    "BorrowerName" text COLLATE pg_catalog."default",
     "BorrowerOKPO" bigint,
-    "BorrowerSector" text,
+    "BorrowerSector" text COLLATE pg_catalog."default",
     "BorrowerUID" integer,
-    "IssuerName" text,
-    "IssuerName_NRD" text,
+    "IssuerName" text COLLATE pg_catalog."default",
+    "IssuerName_NRD" text COLLATE pg_catalog."default",
     "IssuerOKPO" bigint,
     "NumGuarantors" smallint,
     "EndMtyDate" date,
